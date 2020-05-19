772e9f1d752d6c87c60dd9eb118f394e2d037f4c
gautham-lg <30747896+gautham-lg@users.noreply.github.com>
2020-05-19 18:54:03 +0530

    Basic Layout_v1

---
+++ index.js
@@ -0,0 +1,38 @@
+
+
+function Say_oy() {
+	alert("oy")
+}
+
+function Color() {
+	
+	var x = prompt("Enter the color you want to see !!");
+	document.write(x);
+	document.bgColor = x;
+	
+}
+
+function DateShow() {
+	d = new Date();
+	alert(d);
+	
+}
+
+function Name() {
+	
+	var k = document.getElementById("name").value;
+	alert(k);
+}
+
+function func() {
+//$("a.nav-link").css("color","red");	
+//var attr = $("a.nav-link").attr("href");
+
+//if (attr=="index.html"){
+	
+	
+//}
+	
+}
+
+
